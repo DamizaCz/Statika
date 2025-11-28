a620da4e4059f32770d696f3ba6f7833dc2e5046
Damiza <dalizacpal@email.cz>
2025-11-28 08:48:38 +0100

    Druhý committ

--- src/zacpal/cz/Vypocty.java
+++ src/zacpal/cz/Vypocty.java
@@ -3,11 +3,10 @@ package zacpal.cz;
 public class Vypocty {
     public static String lanNetwork (String ipAdressa, String maska){
         String network = "";
-        String [] ip = ipAdressa.split(".");
-        System.out.println(ipAdressa);
-        System.out.println(ip.length);
+        String [] ip = ipAdressa.split("[.]");
+        String [] m = maska.split("[.]");
         for (int i = 0; i < ip.length ; i ++){
-            System.out.println(ip[i]);
+
         }
         return network;
     }
