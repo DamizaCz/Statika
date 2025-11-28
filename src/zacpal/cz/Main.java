f3a202189405ff222dfa89a9adc48258081993fa
Damiza <dalizacpal@email.cz>
2025-11-28 09:28:03 +0100

    DTřetí commit, vše funkční.

--- src/zacpal/cz/Main.java
+++ src/zacpal/cz/Main.java
@@ -2,6 +2,9 @@ package zacpal.cz;
 
 public class Main {
     public static void main(String[] args) {
-    Vypocty.lanNetwork("192.168.1.11","255.255.255.0");
+    String s = Vypocty.lanNetwork("192.168.1.11","255.192.0.0");
+    System.out.println(s);
+    Vypocty v1 = new Vypocty();
+    for (int i=1;i<32;i++) System.out.println(i+ ": " + v1.maska(i));
     }
 }
--- src/zacpal/cz/Vypocty.java
+++ src/zacpal/cz/Vypocty.java
@@ -1,13 +1,45 @@
 package zacpal.cz;
 
 public class Vypocty {
-    public static String lanNetwork (String ipAdressa, String maska){
+    public static String lanNetwork(String ipAdressa, String maska) {
         String network = "";
-        String [] ip = ipAdressa.split("[.]");
-        String [] m = maska.split("[.]");
-        for (int i = 0; i < ip.length ; i ++){
+        int[] networkCislo = new int[4];
+        String[] ip = ipAdressa.split("[.]");
+        String[] m = maska.split("[.]");
 
+        // Pro každou část IP a masky
+        for (int i = 0; i < ip.length; i++) {
+            networkCislo[i] = Integer.parseInt(ip[i]) & Integer.parseInt(m[i]);
+            network += networkCislo[i];
+            // Tečku přidáme pouze mezi oktety
+            if (i < networkCislo.length - 1) {
+                network += ".";
+            }
         }
         return network;
+    }
+
+    public String maska(int pocetBitu) {
+        String maska = "";
+
+        // Přidej 255 pro každou celou oktetu
+        for (int i = 0; i < pocetBitu / 8; i++) {
+            maska += "255.";
+        }
+
+        // Vypočítej hodnotu pro zbytek
+        int[] pom = {128, 64, 32, 16, 8, 4, 2, 1};
+        int sum = 0;
+        for (int i = 0; i < pocetBitu % 8; i++) {
+            sum += pom[i];
+        }
+        maska += sum;
+
+        // Přidej .0 pro zbývající oktety
+        for (int i = 0; i < 3 - pocetBitu / 8; i++) {
+            maska += ".0";
+        }
+
+        return maska;
     }
 }
